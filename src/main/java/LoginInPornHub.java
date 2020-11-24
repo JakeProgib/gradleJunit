7450c297dc4cf87cea433a7a80f224f5859ce8af
= <=>
2020-11-24 11:53:37 +0300

    Edit liph

--- src/main/java/LoginInPornHub.java
+++ src/main/java/LoginInPornHub.java
@@ -4,31 +4,24 @@ import java.io.InputStreamReader;
 
 public class LoginInPornHub {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    public String login(){
-        try {
-            String log = reader.readLine();
-            return  log;
-        } catch (IOException e) {
-            return null;
-        }
-    }
-    public String pass(){
-        try {
-            String pass = reader.readLine();
-            return pass;
-        } catch (IOException e) {
-            return null;
-        }
-    }
-    public boolean checkAge(){
-        try{
-            int age = Integer.parseInt(reader.readLine());
-            if(age>=18){
-                return true;
-            }
-            else{return  false;}
-        } catch (IOException e) {
+    public boolean login(String log){
+        if (log.length() > 0) {
+            return true;
+        } else {
             return false;
         }
+
+    }
+    public boolean pass(String pass){
+        if (pass.length()>6){
+            return true;
+        }
+        else{return false;}
+    }
+    public boolean checkAge(int age){
+        if(age>=18){
+            return true;
+        }
+        else{return  false;}
     }
 }
--- src/test/java/TestLoginInPornHub.java
+++ src/test/java/TestLoginInPornHub.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -5,12 +6,39 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import static org.junit.Assert.assertEquals;
+
 public class TestLoginInPornHub {
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
     LoginInPornHub liph = new LoginInPornHub();
+
     @BeforeClass
     public static void startTesting(){
         System.out.println("Start Testing Class");
     }
+
+    @Test
+    public void testLogin1(){
+        Boolean testLogin = liph.login("Nik");
+        assertEquals(true,testLogin);
+    }
+
+    @Test
+    public void testLogin2(){
+        Boolean testLogin = liph.login("");
+        assertEquals(false,testLogin);
+    }
+    @Test
+    public void testAge(){
+        Boolean testAge = liph.checkAge(32);
+        assertEquals(true, testAge);
+    }
+
 
 }
